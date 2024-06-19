16bb3413491d5bc90680657c501ab5bb45719bff
Ramu Kumar <rk9120541@gmail.com>
2024-06-19 11:33:57 +0530

    Groceries

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,21 @@
+import 'package:groceries/models/category.dart';
+import 'package:groceries/models/grocery.dart';
+import 'package:groceries/data/categories.dart';
+
+var groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:groceries/widgets/grocery_list.dart';
+void main() {
+  runApp(const MaterialApp(
+      home:GroceryList() ,
+  ) );
+}
+
